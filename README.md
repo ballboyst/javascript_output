54995abae6752376507fed4c1fe959f058ffc89a
ballboy <ballboyst365@gmail.com>
2024-11-16 22:57:06 +0900

    要素取得

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # javascript_output
+## 学習内容
+
+### 要素の取得（DOMの操作）
+document.getElementById('id名');
+document.getClassNama('クラス名');
+document.querySelector('要素名orクラス名orID名');
+### 要素の内容操作
+<要素>.innerHTML =  <タグを含めた新しい要素>;
+### 三項演算子
+const <変数名> = 条件式 ? <Trueの処理> : <Falseの処理>;
+### 入力ダイアログの表示
+const <変数名> = windows.prompt(表示文);
--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 'use strict';
 
-// const bmi = window.prompt('BMIの値を入力してください')
-// console.log(`あなたのBMIは${bmi}です`);
-// if (25<bmi) {
-//     console.log('肥満です');
-// }
-// else if (bmi<20) {
-//     console.log('痩せすぎです');
-// }
-// else {
-//     console.log('正常です')
-// };
+const bmi = window.prompt('BMIの値を入力してください')
+console.log(`あなたのBMIは${bmi}です`);
+if (25<bmi) {
+    console.log('肥満です');
+}
+else if (bmi<20) {
+    console.log('痩せすぎです');
+}
+else {
+    console.log('正常です')
+};
 
 document.querySelector('p').textContent = "I am studying JS";
 
