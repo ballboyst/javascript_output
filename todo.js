7e1633d6c0d1c16f7ab81695e221c4930594c31a
ballboy <ballboyst365@gmail.com>
2024-12-15 16:00:29 +0900

    更新

--- todo.js
+++ todo.js
@@ -1,3 +1,8 @@
 "Use strict";
 console.log("表示確認");
+const todoList = [];
 
+ const addButton = document.getElementById('add');
+ const task = document.getElementById('taskName').textContent
+
+addButton.addEventListener('click',()=>{console.log(123)});
