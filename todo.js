a9f6ac7511cbf0ffd3ef641cd415d03965f0e5dc
ballboy <ballboyst365@gmail.com>
2024-12-17 18:31:03 +0900

    add機能実装

--- todo.js
+++ todo.js
@@ -7,9 +7,20 @@ const todoList = [];
  const task = document.getElementById('task');
 
 addButton.addEventListener('click', function(){
-    let li = document.createElement('li');
+    // let li = document.createElement('li');
+    let ul = document.createElement('ul');
+    let check = document.createElement('input');
+    check.setAttribute('type','checkbox');
     let anchor = document.createElement('a');
     anchor.textContent = text.value;
-    li.append(anchor);
-    task.append(li);
+    let updateButton = document.createElement('button');
+    updateButton.textContent = '編集';
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = '削除';
+    // li.append(ul);
+    ul.append(check);
+    ul.append(anchor);
+    ul.append(updateButton);
+    ul.append(deleteButton);
+    task.append(ul);
 });
