b91909b9ee3385c9dbb3f3596f970c628e66ad60
ballboy <ballboyst365@gmail.com>
2025-01-09 23:39:25 +0900

    更新

--- todo-second-challenge.js
+++ todo-second-challenge.js
@@ -1,10 +1,7 @@
 let todoList = [""];
 let taskName = document.getElementById('taskName');
 let add = document.getElementById('add');
-let text = "";
-let task = document.getElementById("task");
-console.log(todoList);
-taskName.addEventListener("change", function(){
+let text = ""; let task = document.getElementById("task"); console.log(todoList); taskName.addEventListener("change", function(){
     addTodo();
 });
 
@@ -26,10 +23,21 @@ const addTodo = () => {
     let deleteButton = document.createElement("button");
     deleteButton.textContent="削除";
     todoList.forEach((todo) => {
+        console.log(todo.todo);
         anchor.textContent = todo.todo;
         task.append(ul);
         ul.append(anchor);
         ul.append(updateButton);
         ul.append(deleteButton);
+        deleteButton.addEventListener('click', function(){
+            // console.log(anchor.textContent)
+            // let array = ["apple", "orange1", "grape2", "great3"];
+            // console.log(array.indexOf("g"));
+            let string= "I love dog!";
+            console.log(string.indexOf("I"));
+            // let index = todoList.findIndex(anchor.textContent);
+            // todoList.splice(index, 1);
+            // console.log(todoList);
+        });
     });
-}
+};
