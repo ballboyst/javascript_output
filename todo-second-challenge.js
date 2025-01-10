95a8effbaaa1865525a1341298eb5e81d855000b
ballboy <ballboyst365@gmail.com>
2025-01-10 23:56:31 +0900

    更新

--- todo-second-challenge.js
+++ todo-second-challenge.js
@@ -1,7 +1,10 @@
-let todoList = [""];
+let todoList = [];
 let taskName = document.getElementById('taskName');
 let add = document.getElementById('add');
-let text = ""; let task = document.getElementById("task"); console.log(todoList); taskName.addEventListener("change", function(){
+let text = "";
+let task = document.getElementById("task");
+console.log(todoList); 
+taskName.addEventListener("change", function(){
     addTodo();
 });
 
@@ -13,31 +16,37 @@ const addTodo = () => {
     };
     todoList.push(obj);
     console.log(todoList);
-    taskName.value = ""; 
-    let ul = document.createElement("ul");
-    let checkbox = document.createElement("input");
-    checkbox.setAttribute("type","checkbox");
-    let anchor = document.createElement("a");
-    let updateButton = document.createElement("button");
-    updateButton.textContent = "編集";
-    let deleteButton = document.createElement("button");
-    deleteButton.textContent="削除";
-    todoList.forEach((todo) => {
-        console.log(todo.todo);
-        anchor.textContent = todo.todo;
-        task.append(ul);
-        ul.append(anchor);
-        ul.append(updateButton);
-        ul.append(deleteButton);
-        deleteButton.addEventListener('click', function(){
-            // console.log(anchor.textContent)
-            // let array = ["apple", "orange1", "grape2", "great3"];
-            // console.log(array.indexOf("g"));
-            let string= "I love dog!";
-            console.log(string.indexOf("I"));
-            // let index = todoList.findIndex(anchor.textContent);
-            // todoList.splice(index, 1);
-            // console.log(todoList);
-        });
-    });
+    taskName.value = "";
+    reload();
 };
+
+const reload = () =>{
+    let ul = document.querySelectorAll('ul');
+    ul.forEach(ul => ul.remove());
+    let cul = document.createElement('ul');
+    
+    for (todo of todoList){
+        let checkbox = document.createElement("input");
+        checkbox.setAttribute("type","checkbox");
+        let anchor = document.createElement("a");
+        anchor.textContent = todo.todo
+        let updateButton = document.createElement("button");
+        updateButton.textContent = "編集";
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent="削除";
+        deleteButton.addEventListener('click', function(){
+            let index = todoList.findIndex(
+                (task) => task.todo === anchor.textContent
+            );
+            todoList.splice(index, 1);
+            console.log(todoList);
+            reload();
+        })
+        cul.append(anchor);
+        cul.append(updateButton);
+        cul.append(deleteButton);
+        task.append(cul)
+        };
+    };
+
+// add delete update は配列の処理のみやって各処理の最後に表示だけするreadを入れるのが効率的か？
